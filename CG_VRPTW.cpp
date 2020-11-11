819596871a8a66bf4f4e1ef84847af63bfa39a2a
along <along1994@sjtu.edu.cn>
2020-11-11 16:06:43 +0800

    2020.11.11.16.06

--- CG_VRPTW.cpp
+++ CG_VRPTW.cpp
@@ -51,7 +51,37 @@ void CG_VRPTW::InitiateRMP(const vector<Route_VRPTW> &initialRoutes, IloObjectiv
 }
 
 
-Solution_VRPTW CG_VRPTW::columnGeneration(const Data_Input_ESPPRC &inputESPPRC, const vector<Route_VRPTW> &initialRoutes, ostream &output) {
+double solveModel(IloCplex &solver) {
+	solver.solve();
+	solver.out() << "solution status = " << solver.getStatus() << endl;
+	solver.out() << "objective = " << solver.getObjValue() << endl;
+	return solver.getObjValue();
+}
+
+
+Solution_VRPTW CG_VRPTW::getAnIntegralSolution(IloModel &modelRMP, IloCplex &solverRMP, IloNumVarArray &X) {
+	Solution_VRPTW sol;
+	sol.reset();
+	try {
+		auto env = modelRMP.getEnv();
+		modelRMP.add(IloConversion(env, X, ILOBOOL));
+		solveModel(solverRMP);
+		if (columns.size() != X.getSize()) throw exception();
+		auto pos = columns.begin();
+		for (int i = 0; i < X.getSize(); ++i, ++pos) {
+			if (equalToReal(solverRMP.getValue(X[i]), 1, PPM)) sol.addRoute(solverRMP.getValue(X[i]), *pos);
+			else if (!equalToReal(solverRMP.getValue(X[i]), 0, PPM)) throw exception();
+		}
+	}
+	catch (const exception &exc) {
+		printErrorAndExit("CG_VRPTW::getAnIntegralSolution", exc);
+	}
+	return sol;
+}
+
+
+Solution_VRPTW CG_VRPTW::columnGeneration(const Data_Input_ESPPRC &inputESPPRC, const vector<Route_VRPTW> &initialRoutes, 
+	const Parameter_CG_VRPTW &prm, ostream &output) {
 	IloEnv env;
 	Solution_VRPTW sol;
 	try {
@@ -72,6 +102,46 @@ Solution_VRPTW CG_VRPTW::columnGeneration(const Data_Input_ESPPRC &inputESPPRC, 
 		// Initiate the model of restricted master problem.
 		InitiateRMP(initialRoutes, objectiveRMP, constraintRMP, X);
 
+		// Define the solver of restricted master problem.
+		IloCplex solverRMP(modelRMP);
+		solverRMP.setOut(env.getNullStream());
+
+		string strLog;
+		int iter = 0;
+		do {
+			// Solve the RMP.
+			strLog = "Solve the master problem for the " + numToStr(++iter) + "th time." + '\n';
+			print(prm.allowPrintLog, output, strLog);
+			solveModel(solverRMP);
+
+			// Get dual values.
+			IloNumArray dualValue(env);
+			solverRMP.getDuals(dualValue, constraintRMP);
+			dualValue[0] = 0;
+
+			// Set reduced cost.
+			Data_Input_ESPPRC input(inputESPPRC);
+			for (int i = 0; i < inputESPPRC.NumVertices; ++i) {
+				for (int j = 0; j < inputESPPRC.NumVertices; ++j) {
+					input.ReducedCost[i][j] = input.RealCost[i][j] - (dualValue[i] + dualValue[j]) / 2;
+				}
+			}
+
+			// Solve the subproblem.
+			Data_Auxiliary_ESPPRC auxiliary;
+			auto resultSP = DPAlgorithmESPPRC(input, auxiliary, output);
+
+			// Stopping criterion for iteration.
+			if (resultSP.empty() || greaterThanReal(resultSP.begin()->getReducedCost(), 0, PPM)) break;
+
+			// Add new columns.
+			for (const auto &elem : resultSP) {
+				addColumn(elem, objectiveRMP, constraintRMP, X);
+			}
+		} while (true);
+
+		// Get an integral solution.
+		sol = getAnIntegralSolution(modelRMP, solverRMP, X);
 	}
 	catch (const exception &exc) {
 		printErrorAndExit("CG_VRPTW::columnGeneration", exc);
--- CG_VRPTW.h
+++ CG_VRPTW.h
@@ -10,7 +10,17 @@ private:
 	vector<pair<double, Route_VRPTW>> routes;
 	double cost;
 public:
+	vector<pair<double, Route_VRPTW>> getRoutes() const { return routes; }
+	double getCost() const { return cost; }
+	void reset() { routes.clear(); cost = 0; }
+	void addRoute(const double weight, const Route_VRPTW &rhs) { routes.push_back(make_pair(weight, rhs)); cost += weight * rhs.getRealCost(); }
+};
 
+
+class Parameter_CG_VRPTW {
+public:
+	// Whether printing is allowed.
+	bool allowPrintLog;
 };
 
 
@@ -21,10 +31,13 @@ public:
 	void clearColumns() { columns.clear(); }
 	void addColumn(const Route_VRPTW &rhs, IloObjective &objectiveRMP, IloRangeArray &constraintRMP, IloNumVarArray &X);
 	void InitiateRMP(const vector<Route_VRPTW> &initialRoutes, IloObjective &objectiveRMP, IloRangeArray &constraintRMP, IloNumVarArray &X);
-	Solution_VRPTW columnGeneration(const Data_Input_ESPPRC &inputESPPRC, const vector<Route_VRPTW> &initialRoutes, ostream &output);
+	Solution_VRPTW getAnIntegralSolution(IloModel &modelRMP, IloCplex &solverRMP, IloNumVarArray &X);
+	Solution_VRPTW columnGeneration(const Data_Input_ESPPRC &inputESPPRC, const vector<Route_VRPTW> &initialRoutes, 
+		const Parameter_CG_VRPTW &prm, ostream &output);
 };
 
 
 unordered_map<int, int> getCount(const vector<int> &vec);
+double solveModel(IloCplex &solver);
 
 
