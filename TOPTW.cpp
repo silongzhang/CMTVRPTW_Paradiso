374b17a3c4cb20cb26a8e8e5ac48954b29867882
silongzhang <silongzhang1314@163.com>
2021-01-19 17:02:44 +0800

    save

--- TOPTW.cpp
+++ TOPTW.cpp
@@ -1,6 +1,49 @@
 #include"TOPTW.h"
 
 
+void TOPTW_CG::addColumn(const Route_VRPTW& rhs, IloObjective& objectiveRMP, IloRangeArray& constraintRMP, IloNumVarArray& X) {
+	try {
+		// Add the column.
+		columns.push_back(rhs);
+
+		// The coefficient in objective function.
+		IloNumColumn col = objectiveRMP(rhs.getRealCost());
+
+		// The coefficient in constraint for limiting the number of vehicles.
+		col += constraintRMP[0](1);
+
+		// The coefficients in constraints for each vertex can be visited at most once.
+		unordered_map<int, int> unMp = getCount(rhs.getPath());
+		for (const auto& elem : unMp) {
+			if (elem.first != 0) {
+				col += constraintRMP[elem.first](1);
+			}
+		}
+
+		// Add the new variable.
+		X.add(IloNumVar(col, 0, IloInfinity, ILOFLOAT));
+
+		col.end();
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("TOPTW_CG::addColumn", exc);
+	}
+}
+
+
+void TOPTW_CG::InitiateRMP(const Parameter_TOPTW_CG& parameter, IloObjective& objectiveRMP, IloRangeArray& constraintRMP, IloNumVarArray& X) {
+	try {
+		clearColumns();
+		for (const auto& rhs : parameter.initialRoutes) {
+			addColumn(rhs, objectiveRMP, constraintRMP, X);
+		}
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("TOPTW_CG::InitiateRMP", exc);
+	}
+}
+
+
 void TOPTW_CG::columnGeneration(const Parameter_TOPTW_CG& parameter, Solution_TOPTW_CG& solution, ostream& output) {
 	IloEnv env;
 	try {
@@ -20,7 +63,7 @@ void TOPTW_CG::columnGeneration(const Parameter_TOPTW_CG& parameter, Solution_TO
 		IloRangeArray constraintRMP = IloAdd(modelRMP, IloRangeArray(env, -IloInfinity, RightSide));
 
 		// Define the variables of restricted master problem.
-		IloNumVarArray Y(env);
+		IloNumVarArray X(env);
 
 
 
--- TOPTW.h
+++ TOPTW.h
@@ -6,6 +6,7 @@
 class Parameter_TOPTW_CG {
 public:
 	Data_Input_VRPTW input_VRPTW;
+	vector<Route_VRPTW> initialRoutes;
 	bool allowPrintLog;
 };
 
@@ -18,6 +19,9 @@ class TOPTW_CG {
 private:
 	list<Route_VRPTW> columns;
 public:
+	void clearColumns() { columns.clear(); }
+	void addColumn(const Route_VRPTW& rhs, IloObjective& objectiveRMP, IloRangeArray& constraintRMP, IloNumVarArray& X);
+	void InitiateRMP(const Parameter_TOPTW_CG& parameter, IloObjective& objectiveRMP, IloRangeArray& constraintRMP, IloNumVarArray& X);
 	void columnGeneration(const Parameter_TOPTW_CG& parameter, Solution_TOPTW_CG& solution, ostream& output);
 };
 
