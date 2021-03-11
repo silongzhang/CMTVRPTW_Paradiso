ea49ce9d333ec48b12d4c01bc678501cd1cc3334
silongzhang <silongzhang1314@163.com>
2021-03-11 16:10:03 +0800

    save

--- BC.cpp
+++ BC.cpp
@@ -1,3 +1,136 @@
 #include"BC.h"
 
 
+bool operator<(const NODE_VRPTW_BC& lhs, const NODE_VRPTW_BC& rhs) {
+	return lhs.priority < rhs.priority;
+}
+
+
+void setConstraintsPartition_VRPTW_BC(const Data_Input_VRPTW& input, const vector<Label_TimePath>& structures, IloModel model, IloNumVarArray X) {
+	try {
+		auto env = model.getEnv();
+		for (int i = 1; i < input.NumVertices; ++i) {
+			IloExpr expr(env);
+			for (int s = 0; s < structures.size(); ++s) {
+				if (structures[s].hasVisited(i)) {
+					expr += X[s];
+				}
+			}
+			model.add(expr == 1);
+			expr.end();
+		}
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("setConstraintsPartition_VRPTW_BC", exc);
+	}
+}
+
+
+void addConstraintsTime_VRPTW_BC(const Parameter_VRPTW_BC& parameter, IloModel model, IloNumVarArray X, 
+	const set<double, timeSortCriterion>& additionalTimes) {
+	try {
+		auto env = model.getEnv();
+		for (const auto t : additionalTimes) {
+			IloExpr expr(env);
+			for (int s = 0; s < parameter.columnPool.size(); ++s) {
+				if (parameter.columnPool[s].strongActive(t)) {
+					expr += X[s];
+				}
+			}
+			model.add(expr <= parameter.input_VRPTW.MaxNumVehicles);
+			expr.end();
+		}
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("addConstraintsTime_VRPTW_BC", exc);
+	}
+}
+
+
+void addConstraintsSR_VRPTW_BC(const Parameter_VRPTW_BC& parameter, IloModel model, IloNumVarArray X,
+	const set<tuple<int, int, int>>& additionalTriplets) {
+	try {
+		auto env = model.getEnv();
+		for (const auto& triplet : additionalTriplets) {
+			IloExpr expr(env);
+			for (int s = 0; s < parameter.columnPool.size(); ++s) {
+				if (parameter.columnPool[s].atLeastTwo(triplet)) {
+					expr += X[s];
+				}
+			}
+			model.add(expr <= 1);
+			expr.end();
+		}
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("addConstraintsSR_VRPTW_BC", exc);
+	}
+}
+
+
+void addConstraintsSFC_VRPTW_BC(IloModel model, IloNumVarArray X, const vector<pair<vector<int>, double>>& SFCSet) {
+	try {
+		auto env = model.getEnv();
+		for (const auto& elem : SFCSet) {
+			IloExpr expr(env);
+			for (const auto i : elem.first) {
+				expr += X[i];
+			}
+			model.add(expr <= elem.second);
+			expr.end();
+		}
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("addConstraintsSFC_VRPTW_BC", exc);
+	}
+}
+
+
+void addConstraintsBranchOnArcs_VRPTW_BC(IloModel model, IloNumVarArray X, const vector<pair<vector<int>, bool>>& branchOnArcs) {
+	try {
+		auto env = model.getEnv();
+		for (const auto& elem : branchOnArcs) {
+			IloExpr expr(env);
+			for (const auto i : elem.first) {
+				expr += X[i];
+			}
+			elem.second ? model.add(expr == 1) : model.add(expr == 0);
+			expr.end();
+		}
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("addConstraintsBranchOnArcs_VRPTW_BC", exc);
+	}
+}
+
+
+void NODE_VRPTW_BC::solve(const Parameter_VRPTW_BC& parameter, ostream& output) {
+	IloEnv env;
+	try {
+		solution.explored = solution.feasible = solution.integer = false;
+		solution.objective = InfinityPos;
+
+		// Define the model.
+		IloModel model(env);
+		IloNumVarArray X(env, parameter.columnPool.size(), 0, 1);
+		setObjective(parameter.columnPool, model, X);
+		setConstraintsPartition_VRPTW_BC(parameter.input_VRPTW, parameter.columnPool, model, X);
+		addConstraintsTime_VRPTW_BC(parameter, model, X, input.timeSet);
+		addConstraintsSR_VRPTW_BC(parameter, model, X, input.tripletSet);
+		addConstraintsSFC_VRPTW_BC(model, X, input.SFCSet);
+		addConstraintsBranchOnArcs_VRPTW_BC(model, X, input.branchOnArcs);
+
+		// Solve the model.
+		IloCplex cplex(model);
+
+
+
+
+
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("NODE_VRPTW_BC::solve", exc);
+	}
+	env.end();
+}
+
--- BC.h
+++ BC.h
@@ -2,7 +2,7 @@
 
 #include"BP.h"
 
-class Parameter_BC {
+class Parameter_VRPTW_BC {
 public:
 	Data_Input_VRPTW input_VRPTW;
 	vector<Label_TimePath> columnPool;
@@ -11,17 +11,20 @@ public:
 	bool allowPrintLog;
 };
 
-class Info_BC {
+class Input_VRPTW_BC {
+public:
+	set<double, timeSortCriterion> timeSet;
+	set<tuple<int, int, int>> tripletSet;
+	vector<pair<vector<int>, double>> SFCSet;
+	vector<pair<vector<int>, bool>> branchOnArcs;
+};
+
+class Info_VRPTW_BC {
 public:
 	int prunedInfeasibility;
 	int prunedInteger;
 	int prunedBound;
 	int branched;
-};
-
-class Parameter_VRPTW_BC {
-public:
-	vector<pair<vector<int>, bool>> branchOnArcs;
 };
 
 class Solution_VRPTW_BC {
@@ -34,4 +37,24 @@ public:
 	vector<Label_TimePath> UB_Integer_Solution;		// The best integer solution found so far.
 };
 
+class NODE_VRPTW_BC {
+	friend bool operator<(const NODE_VRPTW_BC& lhs, const NODE_VRPTW_BC& rhs);
+public:
+	int depth;
+	double priority;
+
+	Input_VRPTW_BC input;
+	Solution_VRPTW_BC solution;
+
+	void solve(const Parameter_VRPTW_BC& parameter, ostream& output);
+	void setPriority(double weightLB, double weightDepth) { priority = weightLB * solution.objective + weightDepth * depth; }
+};
+
+void setConstraintsPartition_VRPTW_BC(const Data_Input_VRPTW& input, const vector<Label_TimePath>& structures, IloModel model, IloNumVarArray X);
+void addConstraintsTime_VRPTW_BC(const Parameter_VRPTW_BC& parameter, IloModel model, IloNumVarArray X, 
+	const set<double, timeSortCriterion>& additionalTimes);
+void addConstraintsSR_VRPTW_BC(const Parameter_VRPTW_BC& parameter, IloModel model, IloNumVarArray X,
+	const set<tuple<int, int, int>>& additionalTriplets);
+void addConstraintsSFC_VRPTW_BC(IloModel model, IloNumVarArray X, const vector<pair<vector<int>, double>>& SFCSet);
+void addConstraintsBranchOnArcs_VRPTW_BC(IloModel model, IloNumVarArray X, const vector<pair<vector<int>, bool>>& branchOnArcs);
 
